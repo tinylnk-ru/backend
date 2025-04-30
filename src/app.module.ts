6c064d3a34450d7232de153d4b3263f1d2ccf168
Artem <112617199+artem-eventagrate@users.noreply.github.com>
2025-04-30 16:54:17 +0600

    Init users module, connected to mysql via typeorm, created user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
+import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+
+dotenv.config();
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: true
+    }),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/users.controller.ts
@@ -0,0 +1,5 @@
+import { Controller, Inject, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+}
