8844ce45850b7429a4df868c7a7ef68e970f22ec
Artem <112617199+artem-eventagrate@users.noreply.github.com>
2025-04-30 16:34:34 +0600

    Added dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
 async function bootstrap() {
+  const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
+  console.log('Application started on port:', PORT);
 }
 bootstrap();
